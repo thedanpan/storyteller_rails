3bc8745e3d5697be80e87e3bfab867351e07c927
Daniella Shin <thedanpan@gmail.com>
2015-02-02 23:38:19 -0500

    add seeds for users, genres, stories, and comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,56 @@ require 'faker'
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# one User has many stories
+# one Genre has many stories
+
+
+# create some genres
+sample_genres = %w(Autobiography Biography Childrens Crime Fantasy Horror Humor Mystery Poetry Romance Scifi Travel)
+
+sample_genres.each do |genre|
+  Genre.create(name: genre)
+end
+
+genres = Genre.all
+
+
+# create some users
+10.times do
+  User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    )
+end
+
+users = User.all
+
+
+
+#create some stories
+for i in users
+  10.times do
+    i.stories.create(
+      title: Faker::Lorem.word,
+      content: Faker::Lorem.paragraph,
+      fiction: [true,false].sample,
+      genre_id: genres.sample.id,
+      user_id: users.sample.id
+      )
+  end
+end
+
+stories = Story.all
+
+# create some comments
+sample_stories = stories.sample(95)
+for i in sample_stories
+  3.times do
+    i.comments.create(
+      content: Faker::Lorem.sentence,
+      story_id: sample_stories.sample.id,
+      commentor_id: users.sample.id
+      )
+  end
+end
